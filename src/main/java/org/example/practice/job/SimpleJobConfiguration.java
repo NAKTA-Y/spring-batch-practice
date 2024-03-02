11bdc6858be02294a523035316704538a7523da6
nakta-y <nakta.y.biz@gmail.com>
2024-03-03 00:27:53 +0900

    job parameters 실습

--- src/main/java/org/example/practice/job/SimpleJobConfiguration.java
+++ src/main/java/org/example/practice/job/SimpleJobConfiguration.java
@@ -4,11 +4,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -26,25 +30,28 @@ public class SimpleJobConfiguration {
     }
 
     @Bean
-    public Step simpleStep1(JobRepository jobRepository, Tasklet myTasklet1, PlatformTransactionManager transactionManager) {
-        return new StepBuilder("simpleStep1", jobRepository)
-                .tasklet(myTasklet1, transactionManager)
-                .build();
-    }
+    @JobScope
+    public Step simpleStep1(JobRepository jobRepository,
+                            PlatformTransactionManager transactionManager,
+                            @Value("#{jobParameters[requestDate]}") String requestDate) {
 
-    @Bean
-    public Tasklet myTasklet1() {
-        return (contribution, chunkContext) -> {
+        Tasklet tasklet = (contribution, chunkContext) -> {
             log.info(
                     """
                             myTasklet1
                             ==============================================
                             >> contribution = {}
                             >> chunkContext = {}
+                            >> requestDate = {}
                             ==============================================
-                            """, contribution, chunkContext
-                    );
+                            """, contribution, chunkContext, requestDate
+            );
+
             return RepeatStatus.FINISHED;
         };
+
+        return new StepBuilder("simpleStep1", jobRepository)
+                .tasklet(tasklet, transactionManager)
+                .build();
     }
 }
