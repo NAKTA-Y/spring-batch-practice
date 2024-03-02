8e0b39bb50992973ab248f5fe2496ea5f3d1ea8f
nakta-y <nakta.y.biz@gmail.com>
2024-03-03 00:57:04 +0900

    step next job 실습

--- src/main/java/org/example/practice/job/SimpleJobConfiguration.java
+++ src/main/java/org/example/practice/job/SimpleJobConfiguration.java
@@ -1,14 +1,11 @@
 package org.example.practice.job;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -18,14 +15,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Slf4j
-@RequiredArgsConstructor
 @Configuration
 public class SimpleJobConfiguration {
 
     @Bean
-    public Job simpleJob(JobRepository jobRepository, Step simpleStep1) {
+    public Job simpleJob(JobRepository jobRepository,
+                         Step simpleStep1,
+                         Step simpleStep2) {
+
         return new JobBuilder("simpleJob", jobRepository)
                 .start(simpleStep1)
+                .next(simpleStep2)
                 .build();
     }
 
@@ -51,6 +51,32 @@ public class SimpleJobConfiguration {
         };
 
         return new StepBuilder("simpleStep1", jobRepository)
+                .tasklet(tasklet, transactionManager)
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep2(JobRepository jobRepository,
+                            PlatformTransactionManager transactionManager,
+                            @Value("#{jobParameters[requestDate]}") String requestDate) {
+
+        Tasklet tasklet = (contribution, chunkContext) -> {
+            log.info(
+                    """
+                            myTasklet2
+                            ==============================================
+                            >> contribution = {}
+                            >> chunkContext = {}
+                            >> requestDate = {}
+                            ==============================================
+                            """, contribution, chunkContext, requestDate
+            );
+
+            return RepeatStatus.FINISHED;
+        };
+
+        return new StepBuilder("simpleStep2", jobRepository)
                 .tasklet(tasklet, transactionManager)
                 .build();
     }
